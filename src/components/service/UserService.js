3c15f518db13f1525d30af824420b9aa14b7df86
Adam Cisar <adamcisar7@gmail.com>
2023-08-06 22:37:15 +0200

    cors policy

--- src/components/service/UserService.js
+++ src/components/service/UserService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8080/api/';
-const API_URL = 'https://spectacular-flan-e470d0.netlify.app/api/';
+const API_URL = 'https://backend-reservation-31b4.onrender.com/api/';
 
 export const signUp =  (firstName, lastName, email, password) => {
   return axios.post(`${API_URL}register`, { firstName, lastName, email, password })
